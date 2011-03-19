f77b3c8c160209c7132276570b7d97ecfc47463f
Victor Farazdagi <simple.square@gmail.com>
2011-03-19 04:28:19 +0300

    Automatic anchor making added

---
+++ scripts/default.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    // add anchors to headers that have IDs set
+    $("#content").children("h1[id], h2[id], h3[id]").each(function (key, el) {
+        $(document.createElement("a"))
+            .attr({
+                title: "Permalink to this section",
+                href: "#" + $(el).attr("id"),
+            })
+            .addClass("anchor")
+            .html("&sect;")
+            .appendTo($(el));
+    });
+});
